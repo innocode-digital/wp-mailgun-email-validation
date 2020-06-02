39abf6c21647ebc4e8abf23a17fe899f8a9f2ad0
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2020-06-02 12:08:54 +0300

    change rest namespace and endpoint

--- wp-mailgun-email-validation.php
+++ wp-mailgun-email-validation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Mailgun Email Validation
  * Description: Validates email address through Mailgun.
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author: Innocode
  * Author URI: https://innocode.com
  * Tested up to: 5.4
